c048327aa5691dd09b146bb2b2da0a7c2d948cea
Furz1q <serbek200@mail.ru>
2024-09-01 04:44:07 +0300

    Начало

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+
+        int os = 1;
+        if (os == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else{
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        System.out.println("Задание 2");
+
+
+
+    }
+}
